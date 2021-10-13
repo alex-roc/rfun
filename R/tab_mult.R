c06dc2adb4b86844f8abd2254ab3b24ce62f4bb3
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2021-10-13 10:34:41 -0400

    add: basic assertion

--- R/tab_mult.R
+++ R/tab_mult.R
@@ -1,5 +1,13 @@
-# tab_mult
-
+#' Crea una tabla de multiplicar
+#'
+#' @param n numeric
+#'
+#' @return matrix
+#' @export
+#'
+#' @examples
+#'tab_mult(10)
 tab_mult <- function(n) {
+  assertive::assert_is_integer(n)
   matrix(rep(1:n, n), n, n, byrow = T) * c(1:n)
 }
