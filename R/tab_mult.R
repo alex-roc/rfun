6828781d4498b35d562f49eec2cb1551b2bc7364
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2021-10-15 22:45:07 -0400

    add: argumento para impresion bonita

--- R/tab_mult.R
+++ R/tab_mult.R
@@ -2,6 +2,7 @@
 #'
 #' @param n (numeric) Hasta qué número se creará la tabla
 #' @param simple (boolean) Solo resultado simple. Falso por defecto
+#' @param bonita (boolean) Si la tabla que se imprime debe tener formato. Verdadero por defecto
 #'
 #' @return matrix
 #' @export
@@ -9,18 +10,23 @@
 #' @examples
 #'tab_mult(10)
 #'tab_mult(5, simple = TRUE)
-tab_mult <- function(n, simple = FALSE) {
+tab_mult <- function(n, simple = FALSE, bonita = TRUE) {
   assertive::assert_is_numeric(n)
 
   prim_fac <- matrix(rep(1:n, n), n, n, byrow = T)
   sec_fac <- c(1:n)
   result <- prim_fac * sec_fac
-  completo <- paste(prim_fac, "X", sec_fac, "=", result)
+  completa <- paste(prim_fac, "x", sec_fac, "=", result)
+  m_completa <- matrix(completa, n, n)
 
   if(simple) {
     return(result)
   } else {
-    return(matrix(completo, n, n))
+    if(bonita) {
+      return(knitr::kable(m_completa, "simple"))
+    } else {
+      return(m_completa)
+    }
   }
 
 }
--- tests/testthat/test-tab_mult.R
+++ tests/testthat/test-tab_mult.R
@@ -1,3 +1,3 @@
 test_that("tabl_mult() crea tabla de multiplicación", {
-  expect_equal(dim(tab_mult(5))[1], 5)
+  expect_equal(dim(tab_mult(5, simple = TRUE, bonita = FALSE))[1], 5)
 })
