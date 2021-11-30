49ba6c4478e5cdd913c5ae0fe8b7b23cb28fbd1d
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2021-11-29 22:46:03 -0400

    add: estructura para descomposicion de binomio

--- R/fact_binom.R
+++ R/fact_binom.R
@@ -1,3 +1,34 @@
+estruc <- list(
+  binom = "",
+  op = "",
+  ter1 = list(
+    ter = "",
+    pot = "",
+    pot_des = c(),
+    var = c(),
+    coef = c(),
+    coef_fac = c(),
+    fac = c()
+  ),
+  ter2 = list(
+    ter = "",
+    pot = "",
+    pot_des = c(),
+    var = c(),
+    coef = c(),
+    coef_fac = c(),
+    fac = c()
+  ),
+  binom_fac = list(
+    com_fac = c(),
+    com_var = c(),
+    rest_1 = c(),
+    rest_2 = c(),
+    result = ""
+  )
+)
+
+
 fact_binom <- function(binomio) {
   op <- stringr::str_extract(binomio, "\\+|-")
   ter_1 <- stringr::str_extract(binomio, "[0-9a-z^]+")
@@ -6,7 +37,5 @@ fact_binom <- function(binomio) {
 
   ter_1_pot <- stringr::str_extract(binomio, "[a-z]\\^[0-9]")
   ter_1_pot_des <- rep(stringr::str_extract(ter_1_pot, "[a-z]",stringr::str_extract(ter_1_pot, "\\d" )))
-  ter_1_var <-
-  ter_1_coef
 
 }
